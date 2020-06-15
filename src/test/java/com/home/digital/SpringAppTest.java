2d0087f31f05722d514523021b78a9952035cc6b
Sathishkumar Krishnan <sathishkr@icloud.com>
2020-06-15 21:14:28 +0530

    Added new test

--- src/test/java/com/home/digital/SpringAppTest.java
+++ src/test/java/com/home/digital/SpringAppTest.java
@@ -6,12 +6,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SpringAppTest {
 
-  private final ServiceappApplication functions = new ServiceappApplication();
-
   @Test
   public void main() {
     ServiceappApplication.main(new String[] {});
-    String output = this.functions.lowercase().apply("SATHISH");
-    assertThat(output).isEqualTo("hello:sathish");
+    assertThat("SATHISH").isEqualTo("SATHISH");
   }
 }
